a24457812483caa965f6bbc0a41c26d7200b7eee
durhamch <durhamch@onid.oregonstate.edu>
2015-12-06 15:57:53 -0600

    Initial code push

---
+++ sessions_demo.js
@@ -0,0 +1,88 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  host  : 'localhost',
+  user  : 'student',
+  password: 'default',
+  database: 'student'
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({secret:'someTempPassword'}));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+
+app.get('/',function(req,res,next){
+  var context = {};
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newUser', context);
+    return;
+  }
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length || 0;
+  context.toDo = req.session.toDo || [];
+  console.log(context.toDo);
+  res.render('toDoList',context);
+});
+
+app.post('/',function(req,res){
+  var context = {};
+
+  if(req.body['New List']){
+    req.session.name = req.body.name;
+    req.session.toDo = [];
+    req.session.curId = 0;
+  }
+
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newUser', context);
+    return;
+  }
+
+  if(req.body['Add Item']){
+    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
+    req.session.curId++;
+  }
+
+  if(req.body['Done']){
+    req.session.toDo = req.session.toDo.filter(function(e){
+      return e.id != req.body.id;
+    })
+  }
+
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length;
+  context.toDo = req.session.toDo;
+  console.log(context.toDo);
+  res.render('toDoList',context);
+});
+
+
+// error handling
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
