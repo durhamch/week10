a51ae83bb5539cc809696bd0273fdf94f2a9470c
durhamch <durhamch@onid.oregonstate.edu>
2015-12-06 18:56:29 -0600

    input form creation

--- workit.js
+++ workit.js
@@ -47,40 +47,6 @@ app.get('/reset-table',function(req,res,next){
     })
   });
 });
-/*
-app.post('/',function(req,res){
-  var context = {};
-
-  if(req.body['New List']){
-    req.session.name = req.body.name;
-    req.session.toDo = [];
-    req.session.curId = 0;
-  }
-
-  //If there is no session, go to the main page.
-  if(!req.session.name){
-    res.render('newUser', context);
-    return;
-  }
-
-  if(req.body['Add Item']){
-    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
-    req.session.curId++;
-  }
-
-  if(req.body['Done']){
-    req.session.toDo = req.session.toDo.filter(function(e){
-      return e.id != req.body.id;
-    })
-  }
-
-  context.name = req.session.name;
-  context.toDoCount = req.session.toDo.length;
-  context.toDo = req.session.toDo;
-  console.log(context.toDo);
-  res.render('toDoList',context);
-});
-*/
 
 // error handling
 app.use(function(req,res){
